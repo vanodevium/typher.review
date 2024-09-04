854c04630d5ace31857a97fab4285a43e93e9a24
Vano Devium <webdevium@gmail.com>
2024-09-04 19:46:44 +0300

    v0.0.1

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
-import './app.css'
+import "./app.css";
 
-import {easyScrollSync} from "easy-scroll-sync";
+import { easyScrollSync } from "easy-scroll-sync";
 
-const Typograf = require('typograf');
+const Typograf = require("typograf");
 
-const typo = new Typograf({locale: ['ru', 'en-US'], live: true});
-const $editor = document.querySelector('#editor');
-const $result = document.querySelector('#result');
+const typo = new Typograf({ locale: ["ru", "en-US"], live: true });
+const $editor = document.querySelector("#editor");
+const $result = document.querySelector("#result");
 
-easyScrollSync()
+easyScrollSync();
 
 $editor.focus();
 
 let lastValue = $editor.value;
 
 $editor.addEventListener(
-  'input',
+  "input",
   async () => {
     const value = $editor.value;
 
     if (value !== lastValue) {
-
       if (!value) {
-        lastValue = '';
+        lastValue = "";
         $result.value = lastValue;
         return;
       }
@@ -32,19 +31,16 @@ $editor.addEventListener(
       await navigator.clipboard.writeText(lastValue);
     }
   },
-  false
+  false,
 );
 
-$result.addEventListener(
-  'click',
-  async () => {
-    await navigator.clipboard.writeText($result.value)
-  }
-)
+$result.addEventListener("click", async () => {
+  await navigator.clipboard.writeText($result.value);
+});
 
 window.dataLayer = window.dataLayer || [];
 window.gtag = function gtag() {
   dataLayer.push(arguments);
-}
-window.gtag('js', new Date());
-window.gtag('config', 'G-X8YC3F72DB');
+};
+window.gtag("js", new Date());
+window.gtag("config", "G-X8YC3F72DB");
