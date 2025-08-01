2bc8537e57e2a6c3c11a83c33c6bde0a3fdd1bed
Vano Devium <webdevium@gmail.com>
2025-08-01 19:25:15 +0300

    v0.0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./src/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
-}
+  plugins: [require("tailwind-scrollbar")],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const path = require('path');
+const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  stats: 'errors-only',
-  context: path.resolve(__dirname, 'src'),
-  entry: './app.js',
+  stats: "errors-only",
+  context: path.resolve(__dirname, "src"),
+  entry: "./app.js",
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'app.[contenthash].js',
-    clean: true
+    path: path.resolve(__dirname, "./dist"),
+    filename: "app.[contenthash].js",
+    clean: true,
   },
-  target: 'web',
+  target: "web",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css'
+      filename: "styles.[contenthash].css",
     }),
     new HtmlWebpackPlugin({
-      template: 'index.html',
-      filename: 'index.html',
+      template: "index.html",
+      filename: "index.html",
     }),
     new CopyPlugin({
       patterns: [
-        { from: './.nojekyll', to: '.' },
-        { from: './*.webmanifest', to: '.' },
-        { from: './*.png', to: '.' },
-        { from: './*.ico', to: '.' },
+        { from: "./.nojekyll", to: "." },
+        { from: "./*.webmanifest", to: "." },
+        { from: "./*.png", to: "." },
+        { from: "./*.ico", to: "." },
       ],
     }),
   ],
@@ -51,12 +51,12 @@ module.exports = {
         },
         extractComments: false,
       }),
-      new CssMinimizerPlugin()
+      new CssMinimizerPlugin(),
     ],
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 1234,
     open: true,
